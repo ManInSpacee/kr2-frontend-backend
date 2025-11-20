bff63b527c5497665022f01a632f1ffc3652d764
ManInSpace <klimentmart@gmail.com>
2025-11-20 14:48:31 +0300

    Accessability update

--- scripts/validation.js
+++ scripts/validation.js
@@ -3,10 +3,10 @@ const forms = document.querySelectorAll('.needs-validation');
 Array.from(forms).forEach(form => {
   form.addEventListener('submit', event => {
     if(!form.checkValidity()) {
-      event.preventDefault()
-      event.stopPropagation()
+      event.preventDefault();
+      event.stopPropagation();
     }
 
-    form.classList.add('was-validated')
-  }, false)
-})
+    form.classList.add('was-validated');
+  }, false);
+});
